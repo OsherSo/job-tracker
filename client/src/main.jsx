b71e1b86e3f6627ae79be3c3daead7d5ce97bc61
Amir Solimnay <amirsoli@post.bgu.ac.il>
2024-06-18 13:45:27 +0300

    Connect register to server

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <ToastContainer position="top-center" />
+  </React.StrictMode>
+);
