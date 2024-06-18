2839a44ec65fc2604a4ec585eb33894864e2c844
Amir Solimnay <amirsoli@post.bgu.ac.il>
2024-06-18 12:32:13 +0300

    Add logout

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 
 import {
   validateRegisterInput,
@@ -11,5 +11,6 @@ const router = Router();
 
 router.post("/register", validateRegisterInput, register);
 router.post("/login", validateLoginInput, login);
+router.get("/logout", logout);
 
 export default router;
